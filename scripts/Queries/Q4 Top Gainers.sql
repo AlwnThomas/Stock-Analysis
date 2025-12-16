c4b0de306ed28d8fd6df8f483ba444c77506d4ba
Alwin Thomas <alwnthomas@gmail.com>
2025-12-16 14:52:31 +0000

    Updated Q4 Top Gainers
    Added insightful queries to Q4

---
+++ scripts/Queries/Q4 Top Gainers.sql
@@ -0,0 +1,51 @@
+-- Identifying the best and worst performers, large daily moves, and cumulative performance.
+
+-- 1. Highest single-day percentage gain across the entire dataset
+SELECT s.ticker, sp.price_date, ((sp.close_price - sp.open_price) / sp.open_price * 100) AS daily_percent_change
+FROM stocks s
+JOIN stock_prices sp
+ ON s.stock_id = sp.stock_id
+ORDER BY daily_percent_change DESC
+LIMIT 3;
+
+-- 2.Largest single-day percentage loss?
+SELECT s.ticker, sp.price_date, ((sp.close_price - sp.open_price) / sp.open_price * 100) AS daily_percent_change
+FROM stocks s
+JOIN stock_prices sp
+ ON s.stock_id = sp.stock_id
+ORDER BY daily_percent_change ASC
+LIMIT 3;
+
+-- 3 Top 3 stocks with the highest daily % gain, for each day
+WITH ranked_gains AS (
+    SELECT 
+        s.ticker,
+        sp.price_date,
+        ((sp.close_price - sp.open_price) / sp.open_price) * 100 AS daily_percent_change,
+        RANK() OVER (
+            PARTITION BY sp.price_date
+            ORDER BY ((sp.close_price - sp.open_price) / sp.open_price) * 100 DESC
+        ) AS gain_rank
+    FROM stocks s
+    JOIN stock_prices sp
+        ON s.stock_id = sp.stock_id
+)
+SELECT
+    ticker,
+    price_date,
+    daily_percent_change
+FROM ranked_gains
+WHERE gain_rank <= 3
+ORDER BY price_date, daily_percent_change DESC;
+
+-- 4. Highest average daily percentage return
+SELECT 
+    s.ticker,
+    AVG((sp.close_price - sp.open_price) / sp.open_price * 100) AS avg_daily_percent_return
+FROM stocks s
+JOIN stock_prices sp
+    ON s.stock_id = sp.stock_id
+GROUP BY s.ticker
+ORDER BY avg_daily_percent_return DESC;
+
+
