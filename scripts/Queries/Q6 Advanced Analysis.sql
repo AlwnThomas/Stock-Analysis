a59af860c6cb714daaef781818e1da666dea1263
Alwin Thomas <alwnthomas@gmail.com>
2025-12-16 20:33:08 +0000

    Update Q6 Advanced Analysis
    Added 2 detailed queries core to business insights

--- scripts/Queries/Q6 Advanced Analysis.sql
+++ scripts/Queries/Q6 Advanced Analysis.sql
@@ -33,3 +33,40 @@ WHERE (sp.high_price - sp.low_price) >
     FROM stock_prices
 )
 ORDER BY daily_volatility DESC;
+
+-- 4. Above-avg perfomance and Below-avg volatility
+WITH stock_metrics AS (
+    SELECT
+        s.ticker,
+        AVG((sp.close_price - sp.open_price) / sp.open_price * 100) AS avg_daily_return,
+        AVG(sp.high_price - sp.low_price) AS avg_daily_volatility
+    FROM stocks s
+    JOIN stock_prices sp
+        ON s.stock_id = sp.stock_id
+    GROUP BY s.ticker
+),
+market_metrics AS (
+    SELECT
+        AVG((close_price - open_price) / open_price * 100) AS market_avg_return,
+        AVG(high_price - low_price) AS market_avg_volatility
+    FROM stock_prices
+)
+SELECT
+    sm.ticker,
+    sm.avg_daily_return,
+    sm.avg_daily_volatility
+FROM stock_metrics sm
+CROSS JOIN market_metrics mm
+WHERE sm.avg_daily_return > mm.market_avg_return
+  AND sm.avg_daily_volatility < mm.market_avg_volatility
+ORDER BY sm.avg_daily_return DESC;
+
+-- 5. Volatility stability
+SELECT
+    s.ticker,
+    STDDEV(sp.high_price - sp.low_price) AS volatility_std_dev
+FROM stocks s
+JOIN stock_prices sp
+    ON s.stock_id = sp.stock_id
+GROUP BY s.ticker
+ORDER BY volatility_std_dev ASC;
